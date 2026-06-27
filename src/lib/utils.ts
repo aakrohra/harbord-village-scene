2a36082f44869aafacfad72f1e63e5264f704d99
aakrohra <aakrohra@gmail.com>
2026-06-27 13:31:49 -0400

    alphabetical album grid

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,4 +2,4 @@ import data from '$lib/data/data.json';
 export const slug = (name: string) => name.toLowerCase().replace(/\s+/g, '-');
 export const allReleases = data.artists.flatMap((artist) =>
     artist.releases.map((release) => ({ ...release, artist }))
-);
+).sort((a, b) => a.title.localeCompare(b.title));
